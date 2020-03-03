f42c44b42d29d8e25d521cc93529e24a8ac2be4f
YourSandwich <norbert23pl@gmail.com>
2020-03-03 08:47:48 +0100

    Würfel4Linux

--- Python/Project/Würfel/Würfel.py
+++ Python/Project/Würfel/Würfel.py
@@ -4,10 +4,10 @@ from random import randint
 
 fenster = Tk()
 fenster.title("Würfel")
-fenster.iconbitmap('D:\Müll\Code\Python\Project\Würfel\calculator.ico')
-fenster.geometry("175x160+800+350")
-fenster.minsize(175, 160)
-fenster.maxsize(175, 160)
+fenster.iconbitmap('@/mnt/DEDC8D3CDC8D1047/Müll/Code/Python/Project/Würfel/calculator.xbm')
+fenster.geometry("210x180+800+350")
+fenster.minsize(220, 180)
+fenster.maxsize(220, 180)
 
 def Zufall():
     zufallszahl = randint(text.get(), text2.get())
@@ -17,21 +17,24 @@ def Zufall():
 text = IntVar()
 text2 = IntVar()
 
+platz= Label(fenster)
+platz.grid(row=0)
+
 lab = Label(fenster, font=("Arial",50), text="?", width = 3)
-lab.grid(row=0, column=1)
+lab.grid(row=1, column=1)
 
 
 von = Entry(fenster, textvariable=text)
-von.grid(row=1, column=1)
+von.grid(row=2, column=1)
 
 bis = Entry(fenster, textvariable=text2)
-bis.grid(row=2, column=1)
+bis.grid(row=3, column=1)
 
-Label(fenster, text="von").grid(row=1)
-Label(fenster, text="bis").grid(row=2)
+Label(fenster, text="von").grid(row=2)
+Label(fenster, text="bis").grid(row=3)
 
 
 knopf = Button(fenster, text = "Drück mich!" , command=Zufall)
-knopf.grid(row=3, column=1)
+knopf.grid(row=4, column=1)
 
 fenster.mainloop()
