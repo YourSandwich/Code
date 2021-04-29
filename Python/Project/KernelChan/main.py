1ea4128dd305528fef3f24d9e67dd93c7e69d623
YourSandwich <norbert23pl@gmail.com>
2021-04-29 17:50:55 +0200

    pyside progress

--- Python/Project/KernelChan/main.py
+++ Python/Project/KernelChan/main.py
@@ -1,4 +1,5 @@
-from PySide2.QtWidgets import QApplication,QWidget
+from PySide2.QtWidgets import QApplication,QWidget,QPushButton,QMessageBox
+from PySide2.QtGui import QIcon
 import sys
 
 class Window(QWidget):
@@ -7,6 +8,27 @@ class Window(QWidget):
 
         self.setWindowTitle("KernelChan")
         self.setGeometry(300,300,300,300)
+
+        self.setIcon()
+        self.setButton()
+
+    def setIcon(self):
+        appIcon = QIcon('icon.png')
+        self.setWindowIcon(appIcon)
+
+    def setButton(self):
+        button = QPushButton("Exit", self)
+        button.move(190,240)
+
+        button.clicked.connect(self.exitApp)
+
+    def exitApp(self):
+        askUser = QMessageBox.question(self, "Quit", "Are you Sure?", QMessageBox.Yes | QMessageBox.No)
+
+        if askUser == QMessageBox.Yes:
+            App.quit()
+        elif askUser == QMessageBox.No:
+            pass
 
 App = QApplication(sys.argv)
 window = Window()
